36607e052165c6d709468540b211c29f5dc73a0b
mika <73530023+2everlove@users.noreply.github.com>
2021-07-15 18:16:13 +0900

    node server

---
+++ server1.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+http.createServer((req, res)=>{
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.write('<h1>Hello Node!</h1>');
+    res.end('<p>Hello Server!</p>');
+}).listen(12500, () => {
+    console.log('ready to server on port 12500');
+});
+
+//http://localhost:12500/
